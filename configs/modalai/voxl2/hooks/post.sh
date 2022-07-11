99556cd18ae693f25704c8c906b868dcb842c158
Christian Stewart <christian@paral.in>
2022-07-11 08:39:35 -0700

    voxl2: fix dtb name
    Signed-off-by: Christian Stewart <christian@paral.in>

--- configs/modalai/voxl2/hooks/post.sh
+++ configs/modalai/voxl2/hooks/post.sh
@@ -65,7 +65,7 @@ echo "Generating $(basename ${BOOT_IMAGE})..."
 KERNEL_CMDLINE="noinitrd rw rootwait console=ttyMSM0,115200,n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 reboot=panic_warm net.ifnames=0"
 ${HOST_DIR}/bin/mkbootimg \
            --kernel Image.gz \
-           --dtb qrb5165-rb5.dtb \
+           --dtb m0054-qrb5165-iot-rb5.dtb \
            --pagesize 4096 \
            --base "0x80000000" \
            --tags_offset "0x81900000" \
